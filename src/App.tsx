e16a6671959a011c6bab311dd78e267a731b2133
gabicilfone <cilfonegabriel@gmail.com>
2024-04-24 09:17:52 -0300

    Creating our first modules of CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-
+import styles from "./App.module.css"
 
 function App() {
 
   return (
     <>
-      <div>Buscador de clima</div>
+      <h1 className={styles.title}>Buscador de Clima</h1>
+
+      <div className={styles.container}>
+        <p>1</p>
+        <p>2</p>
+      </div>
     </>
   )
 }
