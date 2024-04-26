9b535dd10a5ab397833bc36bdf6177f9540bb3ff
gabicilfone <cilfonegabriel@gmail.com>
2024-04-26 20:40:40 -0300

    Creating the environment variable for the API key

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -4,7 +4,7 @@ import { SearchType } from '../types'
 export default function useWeather() {
 
     const fetchWeather = async (search : SearchType) => {
-        const appId = '6b811ade077c4e48a7e6595061c67b17'
+        const appId = import.meta.env.VITE_API_KEY
         try {
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
 
