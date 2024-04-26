5d5b92557bb35069c359ec6267c9aecd2919126e
gabicilfone <cilfonegabriel@gmail.com>
2024-04-26 19:48:33 -0300

    Import custom hook to the app field

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import styles from "./App.module.css"
 import Form from "./components/Form/Form"
+import useWeather from "./hooks/useWeather"
 
 function App() {
-
+  
+  const { fetchWeather } = useWeather()
   return (
     <>
       <h1 className={styles.title}>Buscador de Clima</h1>
 
       <div className={styles.container}>
-        <Form />
-        <p>2</p>
+        <Form 
+          fetchWeather = { fetchWeather }
+        />
       </div>
     </>
   )
