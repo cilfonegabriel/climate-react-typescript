1d84761decd08f0cf4aa3a943508b657e9854770
gabicilfone <cilfonegabriel@gmail.com>
2024-04-26 20:10:49 -0300

    Definig the promp fetchWeather

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -5,7 +5,7 @@ import styles from "./Form.module.css"
 import Alert from "../Alert/Alert";
 
 type FormProps = {
-  fetchWeather: () => void,
+  fetchWeather: (search: SearchType) => Promise<void>,
 }
 
 export default function Form({fetchWeather} : FormProps) {
@@ -32,7 +32,7 @@ export default function Form({fetchWeather} : FormProps) {
       return
     }
 
-    fetchWeather()
+    fetchWeather(search)
   }
 
   return (
