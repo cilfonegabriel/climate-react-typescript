014996375cb6123b11f5f9fc193c44552891d786
gabicilfone <cilfonegabriel@gmail.com>
2024-05-17 18:01:27 -0300

    Tipar Resultados de una API - Validar con ZOD

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,19 @@
 import axios from 'axios'
-import { SearchType, Weather } from '../types'
+import { z } from 'zod'
+import { SearchType } from '../types'
+
+//zod
+const Weather = z.object({
+    name: z.string(),
+    main: z.object({
+        temp: z.number(),
+        temp_max: z.number(),
+        temp_min: z.number(),
+
+    })
+})
+
+type Weather = z.infer<typeof Weather>
 
 export default function useWeather() {
 
@@ -17,10 +31,12 @@ export default function useWeather() {
 
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
             
-            const{ data:weatherResult } = await axios<Weather>(weatherUrl)
-            console.log(weatherResult.main.temp)
-            console.log(weatherResult.name)
-
+            const{ data:weatherResult } = await axios(weatherUrl)
+            const result = Weather.safeParse(weatherResult)
+            if(result.success) {
+                console.log(result.data.name)
+                console.log(result.data.main.temp)
+            }            
         } catch (error) {
             console.log(error)
         }
